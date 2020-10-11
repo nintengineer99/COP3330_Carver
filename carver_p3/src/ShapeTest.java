be9577570546fd21e4b53b7a482accfe7abde186
jake carver <jakeknight99@knights.ucf.edu>
2020-10-10 23:22:55 -0400

    Implemented getName tests for all shapes

--- carver_p3/src/ShapeTest.java
+++ carver_p3/src/ShapeTest.java
@@ -7,4 +7,34 @@ class ShapeTest {
         Shape shape = new Square(10);
         assertEquals("square", shape.getName());
     }
+
+    @Test
+    public void testTriangleName() {
+        Shape shape = new Triangle(100, 100);
+        assertEquals("triangle", shape.getName());
+    }
+
+    @Test
+    public void testCircleName() {
+        Shape shape = new Circle(10);
+        assertEquals("circle", shape.getName());
+    }
+
+    @Test
+    public void testCubeName() {
+        Shape shape = new Cube(10);
+        assertEquals("cube", shape.getName());
+    }
+
+    @Test
+    public void testPyramidName() {
+        Shape shape = new Pyramid(100, 100, 100);
+        assertEquals("pyramid", shape.getName());
+    }
+
+    @Test
+    public void testSphereName() {
+        Shape shape = new Sphere(10);
+        assertEquals("sphere", shape.getName());
+    }
 }
