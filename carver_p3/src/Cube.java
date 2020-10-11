53cb79cdea66f1cd5a8c82cb1716cda0ef34c19e
jake carver <jakeknight99@knights.ucf.edu>
2020-10-10 22:45:56 -0400

    Implemented sphere class by extending Shape3D and related methods

--- carver_p3/src/Cube.java
+++ carver_p3/src/Cube.java
@@ -1,2 +1,22 @@
-public class Cube {
+public class Cube extends Shape3D {
+    private final double length;
+
+    public Cube(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public double getVolume() {
+        return (length * length * length);
+    }
+
+    @Override
+    public String getName() {
+        return "cube";
+    }
+
+    @Override
+    public double getArea() {
+        return (6 * (length * length));
+    }
 }
