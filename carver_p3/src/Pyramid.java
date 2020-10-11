f8b7ea5a324c7a2f806f9d7317487924c5ab4ade
jake carver <jakeknight99@knights.ucf.edu>
2020-10-10 23:08:33 -0400

    Implemented pyramid class by extending Shape3D and related methods

--- carver_p3/src/Pyramid.java
+++ carver_p3/src/Pyramid.java
@@ -1,2 +1,30 @@
-public class Pyramid {
+import java.lang.Math;
+
+public class Pyramid extends Shape3D {
+    private final double baseLength;
+    private final double baseWidth;
+    private final double height;
+
+    public Pyramid(double baseLength, double baseWidth, double height) {
+        this.baseLength = baseLength;
+        this.baseWidth = baseWidth;
+        this.height = height;
+    }
+
+    @Override
+    public double getVolume() {
+        return ((baseLength * baseWidth * height) / 3);
+    }
+
+    @Override
+    public String getName() {
+        return "pyramid";
+    }
+
+    @Override
+    public double getArea() {
+        return ((baseLength * baseWidth)
+                + (baseLength * (Math.sqrt(((0.5 * baseWidth) * (0.5 * baseWidth)) + (height * height))))
+                + (baseWidth * (Math.sqrt(((0.5 * baseLength) * (0.5 * baseLength)) + (height * height)))));
+    }
 }
