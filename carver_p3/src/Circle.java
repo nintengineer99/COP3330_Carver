d48c30639c40dc673fee7342d4dac942ab68fcff
jake carver <jakeknight99@knights.ucf.edu>
2020-10-10 22:35:22 -0400

    Implemented circle class by extending Shape2D and related methods

--- carver_p3/src/Circle.java
+++ carver_p3/src/Circle.java
@@ -1,2 +1,17 @@
-public class Circle {
+public class Circle extends Shape2D {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String getName() {
+        return "circle";
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
 }
