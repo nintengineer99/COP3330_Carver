c5bbf7312c8ed8942f80b7223c17f9d2293ce980
jake carver <jakeknight99@knights.ucf.edu>
2020-10-10 23:41:48 -0400

    Changed pi value from 3.14 to Math.PI

--- carver_p3/src/Sphere.java
+++ carver_p3/src/Sphere.java
@@ -1,2 +1,24 @@
-public class Sphere {
+import java.lang.Math;
+
+public class Sphere extends Shape3D {
+    private final double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getVolume() {
+        return ((4.0 / 3.0) * Math.PI * (radius * radius * radius));
+    }
+
+    @Override
+    public String getName() {
+        return "sphere";
+    }
+
+    @Override
+    public double getArea() {
+        return (4 * Math.PI * (radius * radius));
+    }
 }
