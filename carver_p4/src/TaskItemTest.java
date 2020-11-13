3aa70b7e018184fb1f334d501a68243d7923b439
jake carver <jakeknight99@knights.ucf.edu>
2020-11-12 21:55:31 -0500

    Created necessary tests, begin implementing necessary code

--- carver_p4/src/TaskItemTest.java
+++ carver_p4/src/TaskItemTest.java
@@ -1,2 +1,45 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskItemTest {
+
+    @Test
+    public void creatingTaskItemFailsWithInvalidDueDate() {
+
+    }
+
+    @Test
+    public void creatingTaskItemFailsWithInvalidTitle() {
+
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidDueDate() {
+
+    }
+
+    @Test
+    public void creatingTaskItemSucceedsWithValidTitle() {
+
+    }
+
+    @Test
+    public void settingTaskItemDueDateFailsWithInvalidDate() {
+
+    }
+
+    @Test
+    public void settingTaskItemDueDateSucceedsWithValidDate() {
+
+    }
+
+    @Test
+    public void settingTaskItemTitleFailsWithInvalidTitle() {
+
+    }
+
+    @Test
+    public void settingTaskItemTitleSucceedsWithValidTitle() {
+
+    }
 }
