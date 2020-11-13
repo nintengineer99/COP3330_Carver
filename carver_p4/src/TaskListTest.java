8f53b40f4fd7ff980c2c6319707fce0a89ba0661
jake carver <jakeknight99@knights.ucf.edu>
2020-11-12 22:08:56 -0500

    Created necessary tests, begin implementing necessary code

--- carver_p4/src/TaskListTest.java
+++ carver_p4/src/TaskListTest.java
@@ -1,2 +1,115 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TaskListTest {
+
+    @Test
+    public void addingTaskItemsIncreasesSize() {
+
+    }
+
+    @Test
+    public void completingTaskItemChangesStatus() {
+
+    }
+
+    @Test
+    public void completingTaskItemFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void editingTaskItemChangesValues() {
+
+    }
+
+    @Test
+    public void editingTaskItemDescriptionChangesValue() {
+
+    }
+
+    @Test
+    public void editingTaskItemDescriptionFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void editingTaskItemDueDateChangesValue() {
+
+    }
+
+    @Test
+    public void editingTaskItemDueDateFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void editingTaskItemTitleChangesValue() {
+
+    }
+
+    @Test
+    public void editingTaskItemTitleFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void gettingTaskItemDescriptionFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void gettingTaskItemDescriptionSucceedsWithValidIndex() {
+
+    }
+
+    @Test
+    public void gettingTaskItemDueDateFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void gettingTaskItemDueDateSucceedsWithValidIndex() {
+
+    }
+
+    @Test
+    public void gettingTaskItemTitleFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void gettingTaskItemTitleSucceedsWithValidIndex() {
+
+    }
+
+    @Test
+    public void newTaskListIsEmpty() {
+
+    }
+
+    @Test
+    public void removingTaskItemsDecreasesSize() {
+
+    }
+
+    @Test
+    public void removingTaskItemsFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void savedTaskListCanBeLoaded() {
+
+    }
+
+    @Test
+    public void uncompletingTaskItemChangesStatus() {
+
+    }
+
+    @Test
+    public void uncompletingTaskItemFailsWithInvalidIndex() {
+
+    }
 }
