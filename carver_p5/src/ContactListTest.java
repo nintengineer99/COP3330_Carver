2df5a065220d0f96b2046b6ccdf3158cc87f4a2f
jake carver <jakeknight99@knights.ucf.edu>
2020-11-28 21:10:40 -0500

    Added necessary test cases to be written

--- carver_p5/src/ContactListTest.java
+++ carver_p5/src/ContactListTest.java
@@ -1,2 +1,60 @@
+// library inclusions
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ContactListTest {
+    @Test
+    public void addingItemsIncreasesSize() {
+
+    }
+
+    @Test
+    public void editingItemsFailsWithAllBlankValues() {
+
+    }
+
+    @Test
+    public void editingItemsFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankFirstName() {
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankLastName() {
+
+    }
+
+    @Test
+    public void editingSucceedsWithBlankPhone() {
+
+    }
+
+    @Test
+    public void editingSucceedsWithNonBlankValues() {
+
+    }
+
+    @Test
+    public void newListIsEmpty() {
+
+    }
+
+    @Test
+    public void removingItemsDecreasesSize() {
+
+    }
+
+    @Test
+    public void removingItemsFailsWithInvalidIndex() {
+
+    }
+
+    @Test
+    public void savedContactListCanBeLoaded() {
+        
+    }
 }
