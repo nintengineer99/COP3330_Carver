5778a55e5733e2745b35d3a4bac641be133451db
jake carver <jakeknight99@knights.ucf.edu>
2020-11-29 01:49:53 -0500

    Created editing test cases, implement removing contact capability next.

--- carver_p5/src/ContactItemTest.java
+++ carver_p5/src/ContactItemTest.java
@@ -70,32 +70,93 @@ public class ContactItemTest {
 
     @Test
     public void editingFailsWithAllBlankValues() {
-
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class, () -> {
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contact.setFirstName("");
+                    contact.setLastName("");
+                    contact.setPhoneNumber("");
+                    contact.setEmail("");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankEmail() {
-
+        assertDoesNotThrow( () -> {
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contact.setFirstName("Someone");
+                    contact.setLastName("New");
+                    contact.setPhoneNumber("222-222-2222");
+                    contact.setEmail("");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankFirstName() {
-
+        assertDoesNotThrow( () -> {
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contact.setFirstName("");
+                    contact.setLastName("New");
+                    contact.setPhoneNumber("222-222-2222");
+                    contact.setEmail("example2@example.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankLastName() {
-
+        assertDoesNotThrow( () -> {
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contact.setFirstName("Someone");
+                    contact.setLastName("");
+                    contact.setPhoneNumber("222-222-2222");
+                    contact.setEmail("example2@email.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankPhone() {
-
+        assertDoesNotThrow( () -> {
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contact.setFirstName("Someone");
+                    contact.setLastName("New");
+                    contact.setPhoneNumber("");
+                    contact.setEmail("example2@email.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithNonBlankValues() {
-
+        assertDoesNotThrow( () -> {
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contact.setFirstName("Someone");
+                    contact.setLastName("New");
+                    contact.setPhoneNumber("222-222-2222");
+                    contact.setEmail("example2@email.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
--- carver_p5/src/ContactListTest.java
+++ carver_p5/src/ContactListTest.java
@@ -14,37 +14,109 @@ public class ContactListTest {
 
     @Test
     public void editingItemsFailsWithAllBlankValues() {
-
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class, () -> {
+                    ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(0, "", "", "", "");
+                }
+        );
     }
 
     @Test
     public void editingItemsFailsWithInvalidIndex() {
-
+        Throwable exception = assertThrows(
+                IndexOutOfBoundsException.class, () -> {
+                    ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(1, "", "", "", "");
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankFirstName() {
-
+        assertDoesNotThrow( () -> {
+            ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(0, "", "New",
+                            "222-222-2222", "example2@example.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankLastName() {
-
+        assertDoesNotThrow( () -> {
+                    ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(0, "Someone", "",
+                            "222-222-2222", "example2@example.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithBlankPhone() {
+        assertDoesNotThrow( () -> {
+                    ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(0, "Someone", "New",
+                            "", "example2@example.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
+    }
 
+    @Test
+    public void editingSucceedsWithBlankEmail() {
+        assertDoesNotThrow( () -> {
+                    ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(0, "Someone", "New",
+                            "222-222-2222", "");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void editingSucceedsWithNonBlankValues() {
-
+        assertDoesNotThrow( () -> {
+                    ContactList contactList = new ContactList();
+                    ContactItem contact = new ContactItem("Example", "Contact",
+                            "111-111-1111", "example@email.com");
+                    contactList.addContact(contact);
+                    contactList.editContact(0, "Someone", "New",
+                            "222-222-2222", "example2@example.com");
+                    contact.checkContactValidity(contact.firstName, contact.lastName, contact.phoneNumber,
+                            contact.email);
+                }
+        );
     }
 
     @Test
     public void newListIsEmpty() {
-
+        ContactList contactList = new ContactList();
+        assertEquals(0, contactList.contactCount);
     }
 
     @Test
